b9a2c051e81633c614822082299522180b05630c
Lonli-Lokli <lonli.lokli@gmail.com>
2019-06-24 01:11:21 -0600

    exampe added

---
+++ MiddlewareExample/AutorunService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace MiddlewareExample
+{
+    public class AutorunService : IHostedService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private int _counter = 1;
+        private Timer _timer;
+
+        /// <inheritdoc />
+        public AutorunService(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        /// <inheritdoc />
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(OnCheck, null, 10000, 10000);
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc />
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        private async void OnCheck(object state)
+        {
+            var currentJobId = _counter++;
+            try
+            {
+                Console.WriteLine($"Starting job # {currentJobId}...");
+                var client = _clientFactory.CreateClient();
+                var result = await client.GetStringAsync("http://localhost:5000/api/values");
+                Console.WriteLine($"Job # {currentJobId} completed successfully: {result}");
+                Console.WriteLine("Success!!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Job # {currentJobId} FAILED: {e}");
+            }
+        }
+    }
+}
---
+++ MiddlewareExample/CustomAuthSchema.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
+
+namespace MiddlewareExample
+{
+    public class CustomAuthSchemaDefaults
+    {
+        public const string AuthenticationScheme = "Custom";
+        public const string ADMIN = "Admin";
+
+        public static AuthenticationTicket CreateTicket()
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "test")
+            };
+
+            claims.Add(new Claim(ClaimTypes.Role, ADMIN));
+
+            var claimsIdentity = new ClaimsIdentity(claims, AuthenticationScheme);
+
+            return new AuthenticationTicket(
+                new ClaimsPrincipal(claimsIdentity),
+                new AuthenticationProperties { AllowRefresh = false },
+                AuthenticationScheme);
+        }
+    }
+
+    public class CustomAuthSchemaOptions : AuthenticationSchemeOptions { }
+
+    public class CustomAuthSchemaHandler : AuthenticationHandler<CustomAuthSchemaOptions>
+    {
+        /// <inheritdoc />
+        public CustomAuthSchemaHandler(IOptionsMonitor<CustomAuthSchemaOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var handler = this;
+            string header = handler.Request.Headers[HeaderNames.Authorization];
+            if (string.IsNullOrEmpty(header))
+            {
+                return AuthenticateResult.NoResult();
+            }
+
+            if (header.ToUpperInvariant().StartsWith("BEARER"))
+            {
+                return AuthenticateResult.Success(CustomAuthSchemaDefaults.CreateTicket());
+            }
+
+            return AuthenticateResult.NoResult();
+
+        }
+
+        /// <inheritdoc />
+        protected override async Task HandleChallengeAsync(AuthenticationProperties properties)
+        {
+            var handler = this;
+            handler.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            handler.Response.Headers[HeaderNames.WWWAuthenticate] =
+                CustomAuthSchemaDefaults.AuthenticationScheme;
+        }
+    }
+}
